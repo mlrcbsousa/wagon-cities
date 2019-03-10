12b9f69c2f3515907793898836012400845aba28
Manuel Sousa <mlrcbsousa@gmail.com>
2019-03-10 23:44:04 +0100

    image fitting inside div

--- src/containers/active_city.jsx
+++ src/containers/active_city.jsx
@@ -13,7 +13,7 @@ class ActiveCity extends Component {
         <div className="active-city">
           <h3>{name}</h3>
           <p>{address}</p>
-          <img src={src} alt="" />
+          <img src={src} alt="" style={{ width: '100%' }} />
         </div>
       );
     }
